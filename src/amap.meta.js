6559ee13884450b3e9fd0a064149328854ef5888
Eric Moore <EricGittaMoore@duck.com>
2025-02-19 20:52:15 +0800

    feat(src/amap.meta.js): 添加高德地图增强工具元数据

---
+++ src/amap.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name            高德地图-增强工具
+// @namespace       https://github.com/eric-gitta-moore/amap-enhanced
+// @version         2025-02-18
+// @description     高德地图添加绘制功能，显示收藏图片等
+// @author          https://eric-gitta-moore.github.io/
+// @match           https://www.amap.com/*
+// @icon            https://a.amap.com/pc/static/favicon.ico
+// @grant           none
+// @run-at          document-start
+// ==/UserScript==
