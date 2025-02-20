e56ea6d877540eb7a209b65bb4678a00f9639e2e
Eric Moore <EricGittaMoore@duck.com>
2025-02-21 01:15:25 +0800

    build: 更新版本号并添加依赖库

--- src/amap.meta.js
+++ src/amap.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            高德地图增强插件 - 为高德地图网页版添加更多实用功能
 // @namespace       https://github.com/eric-gitta-moore/amap-enhanced
-// @version         2025.02.21.1
+// @version         2025.02.21.2
 // @description     高德地图增强插件 - 为高德地图网页版添加更多实用功能
 // @author          https://eric-gitta-moore.github.io/
 // @match           https://www.amap.com/*
@@ -11,6 +11,9 @@
 // @run-at          document-start
 // @require         https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.js
 // @require         https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.min.js
+// @require         https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js
+// @require         https://unpkg.com/lucide@0.475.0/dist/umd/lucide.min.js
+// @require         https://unpkg.com/watchjs@0.0.0/src/watch.min.js
 // @resource        toastify.min.css https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.css
 // @resource        viewer.min.css https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.min.css
 // @downloadURL     https://github.com/eric-gitta-moore/amap-enhanced/raw/main/src/amap.user.js
