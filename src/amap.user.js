54e878d4ff5f4dba93d3cb67cb4d197d4c52d1d4
Eric Moore <EricGittaMoore@duck.com>
2025-02-21 12:57:08 +0800

    feat(amap): 添加layer资源及优化依赖加载

--- src/amap.user.js
+++ src/amap.user.js
@@ -10,12 +10,13 @@
 // @grant           GM_getResourceText
 // @run-at          document-start
 // @require         https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.js
+// @resource        toastify.min.css https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.css
 // @require         https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.min.js
-// @require         https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js
+// @resource        viewer.min.css https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.min.css
+// @require         https://cdnjs.cloudflare.com/ajax/libs/layer/3.5.1/layer.min.js
+// @resource        layer.min.css https://cdnjs.cloudflare.com/ajax/libs/layer/3.5.1/theme/default/layer.min.css
 // @require         https://unpkg.com/lucide@0.475.0/dist/umd/lucide.min.js
 // @require         https://unpkg.com/watchjs@0.0.0/src/watch.min.js
-// @resource        toastify.min.css https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.css
-// @resource        viewer.min.css https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.min.css
 // @downloadURL     https://github.com/eric-gitta-moore/amap-enhanced/raw/main/src/amap.user.js
 // @updateURL       https://github.com/eric-gitta-moore/amap-enhanced/raw/main/src/amap.meta.js
 // ==/UserScript==
